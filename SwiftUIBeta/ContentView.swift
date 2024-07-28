d8f1d6de019cdd5b63765671be33282e14fab6ea
JuanDa <juandarm02@gmail.com>
2024-07-29 00:52:22 +0200

    Initial Commit

---
+++ SwiftUIBeta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBeta
+//
+//  Created by JuanDa on 29/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBeta/SwiftUIBetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBetaApp.swift
+//  SwiftUIBeta
+//
+//  Created by JuanDa on 29/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
